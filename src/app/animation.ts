8ebd3b3f3ebe25ee3a92d903a25aeef3c6103660
Luis Fernandez <fernandezluisn@gmail.com>
2020-06-05 01:29:39 +0200

    correccion animaciones

--- src/app/animation.ts
+++ src/app/animation.ts
@@ -26,7 +26,7 @@ export const slideInAnimation =
       ]),
       query(':enter', animateChild()),
     ]),
-    transition(' => ', [
+    transition(' Turnos => Login', [
       style({ position: 'relative' }),
       query(':enter, :leave', [
         style({
