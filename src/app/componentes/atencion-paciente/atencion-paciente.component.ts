96f6a0c15836eda508803a9f2d827dcef76619fb
Luis Fernandez <fernandezluisn@gmail.com>
2020-08-27 19:21:32 -0300

    punto5

--- src/app/componentes/atencion-paciente/atencion-paciente.component.ts
+++ src/app/componentes/atencion-paciente/atencion-paciente.component.ts
@@ -36,19 +36,49 @@ export class AtencionPacienteComponent implements OnInit {
   temperatura:number;
   presion:number;
 
-  dato1:any;
-  dato2:any;
-  dato3:any;
+ 
+
+  control1;
+  control2;
+  control3;
+  listaControles=["Select", "Rango", "Número"];
+  listaSelect=["Si","No"];
+
+  
+
+  numero1:boolean;
+  numero2:boolean;
+  numero3:boolean;
+
+  select1:boolean;
+  select2:boolean;
+  select3:boolean;
+
+  rango1:boolean;
+  rango2:boolean;
+  rango3:boolean;
 
   dato1n:string;
   dato2n:string;
   dato3n:string;
 
+  select1R="Si";
+  rango1R=0;
+  numero1R:number;
+
+  select2R="Si";
+  rango2R=0;
+  numero2R:number;
+
+  select3R="Si";
+  rango3R=0;
+  numero3R:number;
+
   n1=false;
   n2=false;
   n3=false;
 
-  contador=0;
+  
 
  
 
@@ -57,6 +87,19 @@ export class AtencionPacienteComponent implements OnInit {
     this.edad=30;
     this.temperatura=36.1;
     this.presion=14.3;
+
+    this.control1="Select";
+    this.numero1=false;
+    this.select1=true;
+    this.rango1=false;
+    this.control2="Select";
+    this.numero2=false;
+    this.select2=true;
+    this.rango2=false;
+    this.control3="Select";
+    this.numero3=false;
+    this.select3=true;
+    this.rango3=false;
 
     this.txtResenia=null;
     this.service.tomarUsuario().then(element=>{
@@ -149,27 +192,26 @@ export class AtencionPacienteComponent implements OnInit {
       this.turnoACompletar.presión=this.presion;
       this.turnoACompletar.edad=this.edad;
       this.turnoACompletar.temperatura=this.temperatura;
-      switch(this.contador){
-        case(1):
-          this.turnoACompletar.dato1n=this.dato1n;
-          this.turnoACompletar.dato1v=this.dato1;
-        break;
-        case(2):
-          this.turnoACompletar.dato1n=this.dato1n;
-          this.turnoACompletar.dato1v=this.dato1;
-          this.turnoACompletar.dato2n=this.dato2n;
-          this.turnoACompletar.dato2v=this.dato2;
-        break;
-        case(3):
-        console.log(this.dato1n);
+      if(this.n1 && this.n2 && this.n3){
         this.turnoACompletar.dato1n=this.dato1n;
-          this.turnoACompletar.dato1v=this.dato1;
-          this.turnoACompletar.dato2n=this.dato2n;
-          this.turnoACompletar.dato2v=this.dato2;
-          this.turnoACompletar.dato3n=this.dato3n;
-          this.turnoACompletar.dato3v=this.dato3;          
-        break;        
-      }  
+        this.llenar1();
+        this.turnoACompletar.dato2n=this.dato2n;
+        this.llenar2();
+        this.turnoACompletar.dato3n=this.dato3n;
+        this.llenar3();  
+      }else if(this.n1 && this.n2){
+        this.turnoACompletar.dato1n=this.dato1n;
+        this.llenar1();
+        this.turnoACompletar.dato2n=this.dato2n;
+        this.llenar2();
+        
+      }else if(this.n1){
+        this.turnoACompletar.dato1n=this.dato1n;
+        this.llenar1();
+      }        
+       
+              
+       
     }
     
 
@@ -185,32 +227,107 @@ export class AtencionPacienteComponent implements OnInit {
     
   }
 
+  llenar1(){
+    if(this.select1)
+    this.turnoACompletar.dato1v=this.select1R;
+    else if(this.numero1)
+    this.turnoACompletar.dato1v=this.numero1R;
+    else
+    this.turnoACompletar.dato1v=this.rango1R;
+  }
+
+  llenar2(){
+    if(this.select2)
+    this.turnoACompletar.dato2v=this.select2R;
+    else if(this.numero2)
+    this.turnoACompletar.dato2v=this.numero2R;
+    else
+    this.turnoACompletar.dato2v=this.rango2R;
+
+    console.log(this.turnoACompletar.dato2v);
+  }
+
+  llenar3(){
+    if(this.select3)
+    this.turnoACompletar.dato3v=this.select3R;
+    else if(this.numero3)
+    this.turnoACompletar.dato3v=this.numero3R;
+    else
+    this.turnoACompletar.dato3v=this.rango3R;
+  }
+
+  control1a(){
+    switch(this.control1){
+      case "Select":
+        this.numero1=false;
+        this.select1=true;
+        this.rango1=false;
+        break;
+      case "Rango":
+        this.numero1=false;
+        this.select1=false;
+        this.rango1=true;
+        break;
+      case "Número":
+        this.numero1=true;
+        this.select1=false;
+        this.rango1=false;
+        break;
+    }
+  }
+
+  control1b(){
+    switch(this.control2){
+      case "Select":
+        this.numero2=false;
+        this.select2=true;
+        this.rango2=false;
+        break;
+      case "Rango":
+        this.numero2=false;
+        this.select2=false;
+        this.rango2=true;
+        break;
+      case "Número":
+        this.numero2=true;
+        this.select2=false;
+        this.rango2=false;
+        break;
+    }
+  }
+
+  control1c(){
+    switch(this.control3){
+      case "Select":
+        this.numero3=false;
+        this.select3=true;
+        this.rango3=false;
+        break;
+      case "Rango":
+        this.numero3=false;
+        this.select3=false;
+        this.rango3=true;
+        break;
+      case "Número":
+        this.numero3=true;
+        this.select3=false;
+        this.rango3=false;
+        break;
+    }
+  }
+
   agregarDatos(){
     this.seAgregaronDatos=true;
   }
 
   sumarDato(){
-    if(this.contador<3){
-      console.log(this.contador);
-
-      switch(this.contador){
-        case(0):
-        this.n1=true;
-        this.n2=false;
-        this.n3=false;
-        break;
-        case(1):
-        this.n1=true;
-        this.n2=true;
-        this.n3=false;
-        case(2):
-        this.n1=true;
-        this.n2=true;
-        this.n3=true;
-      }
-      this.contador++;
+    if(this.n1==false){
+      this.n1=true;
+    }else if (this.n2==false)
+    {
+      this.n2=true;
     }else{
-      alert("No puede agregar más de 3 nuevas categorías.");
+      this.n3=true;
     }
     
   }
